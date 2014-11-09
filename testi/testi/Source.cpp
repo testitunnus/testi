a71dfd3f75afcdbc47cb426ed040f227f3395c0c
testitunnus <yqweqqpb@sharklasers.com>
2014-11-09 20:06:13 +0200

    added name query

--- testi/testi/Source.cpp
+++ testi/testi/Source.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
+#include <string>
 
 int main() {
 	std::cout << "Hello world" << std::endl;
+	std::string name;
+	std::cout << "Enter name: ";
+	std::cin >> name;
+	std::cout << "Hello " << name << std::endl;
 	return 0;
 }
